786fcaca70ca138fad4a22f31cbace2a54c4b191
mherman-professional <38703102+mherman-professional@users.noreply.github.com>
2018-04-26 11:14:07 -0700

    Server credentials test

---
+++ js/config.js
@@ -0,0 +1,12 @@
+/*
+    var SERVER_BASE_URL = '';
+    fetch(SERVER_BASE_URL + '/session').then(function(res) {
+      return res.json()
+    }).then(function(res) {
+      apiKey = res.apiKey;
+      sessionId = res.sessionId;
+      token = res.token;
+      initializeSession();
+    }).catch(handleError);
+
+*/
